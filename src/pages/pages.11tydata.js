5204bb8b7c071082370c74c36a3fb90346f8e26b
Gregory Korte <gregorykorte@gmail.com>
2025-09-01 22:33:17 -0400

    Add About page.

---
+++ src/pages/pages.11tydata.js
@@ -0,0 +1,9 @@
+export default {
+  layout: "layouts/base.hbs",
+  eleventyComputed: {
+    // Pretty URLs: /<slug>/
+    permalink: (data) => `/${data.page.fileSlug}/`,
+    // Optional: expose a 'page' tag for filtering or nav
+    tags: ["page"]
+  }
+};
