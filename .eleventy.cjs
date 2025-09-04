1d4cc85863927aad5a908895417026527ba97a66
Gregory Korte <gregorykorte@gmail.com>
2025-09-03 21:15:55 -0400

    Fix 404s and relative paths.

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,6 +1,7 @@
 // .eleventy.js
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/static": "static" }); // → _site/static/*
+  eleventyConfig.addPassthroughCopy({ "src/static/assets": "assets" });
   return {
     dir: { input: "src", includes: "_includes", layouts: "_layouts", data: "_data", output: "_site" },
   };
