f135834f888916373a6797c8af7944306272691c
Gregory Korte <gregorykorte@gmail.com>
2026-01-18 16:09:00 -0500

    Publish admin directory to site output

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,13 @@ import EleventyHandlebars from "@11ty/eleventy-plugin-handlebars";
 import handlebars from "handlebars";
 
 export default function (eleventyConfig) {
+
+    // Static assets passthrough: src/static/* → /
+  eleventyConfig.addPassthroughCopy({ "src/static": "/" });
+
+  // Admin CMS passthrough: /admin → /admin
+  eleventyConfig.addPassthroughCopy({ "admin": "admin" });
+
   // Handlebars templating
   eleventyConfig.addPlugin(EleventyHandlebars, { handlebars });
 
