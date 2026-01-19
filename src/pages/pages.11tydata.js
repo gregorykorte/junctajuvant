a8ddd6e16cc9ab7995f9a9b391fda2b482e13bac
Gregory Korte <gregorykorte@gmail.com>
2026-01-18 20:46:09 -0500

    Add page layout with margins for static pages

--- src/pages/pages.11tydata.js
+++ src/pages/pages.11tydata.js
@@ -1,5 +1,9 @@
 // src/pages/pages.11tydata.js
 export default {
+  // Default layout for all pages in src/pages
+  layout: "page",
+
+  // Control where pages are written
   permalink: (data) => {
     const slug = (data.slug || "").trim();
     const fileSlug = data.page?.fileSlug;
